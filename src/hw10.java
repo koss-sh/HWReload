e7e10be361268a340a6ae1e1d5b23fc2b206ff23
koss-sh <shilinskii22@gmail.com>
2023-10-24 14:03:22 +0300

    hw10 done

---
+++ src/hw10.java
@@ -0,0 +1,19 @@
+public class hw10 {
+    public static void main(String[] args) {
+
+        System.out.println("task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("task 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("task 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace('ё', 'е'));
+
+    }
+}
