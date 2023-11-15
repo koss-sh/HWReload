7ff0db37e2e06a0814c4ec4c706254a6ec9f57b9
koss-sh <shilinskii22@gmail.com>
2023-11-15 17:05:07 +0300

    cw1.2 done

--- src/cw1/EmployeeBook.java
+++ src/cw1/EmployeeBook.java
@@ -1,5 +1,7 @@
 package cw1;
 
+import java.sql.SQLOutput;
+
 public class EmployeeBook {
     static Employee[] emps;
     static int size;
@@ -32,8 +34,38 @@ public class EmployeeBook {
         System.out.println("Минимальная ЗП: " + EmployeeBookService.minSalary());
         System.out.println("Максимальная ЗП: " + EmployeeBookService.maxSalary());
         System.out.printf("Средняя ЗП: %.2f%n", EmployeeBookService.avgSalary());
+        EmployeeBookService.indexSalary(10);
+        System.out.println("После индексации:");
+        System.out.println("Сумма всех ЗП: " + EmployeeBookService.salarySum());
+        System.out.println("Минимальная ЗП: " + EmployeeBookService.minSalary());
+        System.out.println("Максимальная ЗП: " + EmployeeBookService.maxSalary());
+        System.out.printf("Средняя ЗП: %.2f%n", EmployeeBookService.avgSalary());
+        int department = 1;
+        System.out.printf("Минимум в отделе №%d у сотрудника: %s%n",
+                department, EmployeeBookService.minSalary(department));
+        System.out.printf("Максимум в отделе №%d у сотрудника: %s%n",
+                department, EmployeeBookService.maxSalary(department));
+        System.out.printf("Сумма ЗП по отделу №%d у сотрудника: %s%n",
+                department, EmployeeBookService.salarySum(department));
+        System.out.printf("Средняя ЗП по отделу №%d: %.2f%n", department, EmployeeBookService.avgSalary(department));
+        EmployeeBookService.indexSalary(department,20);
+        System.out.printf("После индексации отдела №%d:%n", department);
+        System.out.printf("Минимум в отделе №%d у сотрудника: %s%n",
+                department, EmployeeBookService.minSalary(department));
+        System.out.printf("Максимум в отделе №%d у сотрудника: %s%n",
+                department, EmployeeBookService.maxSalary(department));
+        System.out.printf("Сумма ЗП по отделу №%d у сотрудника: %s%n",
+                department, EmployeeBookService.salarySum(department));
+        System.out.printf("Средняя ЗП по отделу №%d: %.2f%n", department, EmployeeBookService.avgSalary(department));
+        System.out.printf("ФИО сотрудников отдела №%d:", department);
+        EmployeeBookService.getEmpNames(department);
+        System.out.println("Все с ЗП меньше 100000: ");
+        EmployeeBookService.getSalaryLessThan(100000);
+        System.out.println("Все с ЗП больше 100000: ");
+        EmployeeBookService.getSalaryMoreThan(100000);
         System.out.println("ФИО сотрудников:");
-        EmployeeBookService.getEmpNames();
+        //EmployeeBookService.getEmpNames();
+
     }
 
 }
--- src/cw1/EmployeeBookService.java
+++ src/cw1/EmployeeBookService.java
@@ -62,10 +62,108 @@ public class EmployeeBookService {
     public static double avgSalary() {
         return (double) salarySum() / (Employee.getCount() - 1);
     }
+
     public static void getEmpNames() {
         for (Employee e : emps) {
             if (e != null) {
                 System.out.println(e.getName());
+            }
+        }
+    }
+
+    // СРЕДНИЙ УРОВЕНЬ
+    public static void indexSalary(double indexationPercent) {
+        double indexationCoeff = 1 + indexationPercent / 100;
+        for (Employee e : emps) {
+            if (e != null) {
+                e.setSalary((int) (e.getSalary() * indexationCoeff));
+            }
+        }
+    }
+
+    public static Employee minSalary(int department) {
+        int min = Integer.MAX_VALUE;
+        Employee emp = null;
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                if (e.getSalary() < min) {
+                    min = e.getSalary();
+                    emp = e;
+                }
+            }
+        }
+        return emp;
+    }
+
+    public static Employee maxSalary(int department) {
+        int max = Integer.MIN_VALUE;
+        Employee emp = null;
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                if (e.getSalary() > max) {
+                    max = e.getSalary();
+                    emp = e;
+                }
+            }
+        }
+        return emp;
+    }
+
+    public static int salarySum(int department) {
+        int sum = 0;
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                sum += e.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static double avgSalary(int department) {
+        int sum = 0;
+        int count = 0;
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                count++;
+                sum += e.getSalary();
+            }
+        }
+        if (count == 0) return 0;
+        return (double) sum / count;
+    }
+    public static void indexSalary(int department, double indexationPercent) {
+        double indexationCoeff = 1 + indexationPercent / 100;
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                e.setSalary((int) (e.getSalary() * indexationCoeff));
+            }
+        }
+    }
+    public static void getEmpNames(int department) {
+        for (Employee e : emps) {
+            if (e != null && e.getDepartment() == department) {
+                System.out.printf(
+                        "ID: %d, ФИО: %s, ЗП: %d%n",
+                        e.getId(), e.getName(), e.getSalary()
+                );
+            }
+        }
+    }
+    public static void getSalaryLessThan(int salary) {
+        for (Employee e : emps) {
+            if (e != null && e.getSalary() < salary) {
+                System.out.printf(
+                        "ID: %d, ФИО: %s, ЗП: %d%n",
+                        e.getId(), e.getName(), e.getSalary());
+            }
+        }
+    }
+    public static void getSalaryMoreThan(int salary) {
+        for (Employee e : emps) {
+            if (e != null && e.getSalary() > salary) {
+                System.out.printf(
+                        "ID: %d, ФИО: %s, ЗП: %d%n",
+                        e.getId(), e.getName(), e.getSalary());
             }
         }
     }
